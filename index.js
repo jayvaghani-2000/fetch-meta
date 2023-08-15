18d4d808e3c07bd81ab576821c2a32ab9be70f63
Jay vaghani <jayvaghani2000@gmail.com>
2023-08-15 13:05:22 +0530

    fetch meta image

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
-
-const playwright = require("playwright");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
-app.get("/fetch-data", async (req, res) => {
+app.get("/fetch-title", async (req, res) => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -49,6 +49,127 @@ app.get("/fetch-data", async (req, res) => {
     res.status(500).json({ error: "An error occurred." });
   }
 });
+app.get("/fetch-keywords", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    const url = req.query.url; // Get the URL from the query parameter
+    if (!url) {
+      res.status(400).json({ error: "URL parameter is required." });
+      return;
+    }
+
+    await page.goto(url);
+    const keywordsSelector = 'meta[name="keywords"]';
+
+    await page.waitForSelector(keywordsSelector); // Wait for the meta keywords tag to appear
+
+    const keywords = await page.$eval(keywordsSelector, (element) =>
+      element.getAttribute("content")
+    );
+
+    await browser.close();
+
+    res.json({
+      keywords,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred." });
+  }
+});
+app.get("/fetch-image", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    const url = req.query.url; // Get the URL from the query parameter
+    if (!url) {
+      res.status(400).json({ error: "URL parameter is required." });
+      return;
+    }
+
+    await page.goto(url);
+    const imageSelector = 'meta[property="og:image"]';
+
+    await page.waitForSelector(imageSelector); // Wait for the meta keywords tag to appear
+
+    const image = await page.$eval(imageSelector, (element) =>
+      element.getAttribute("content")
+    );
+
+    await browser.close();
+
+    res.json({
+      image,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred." });
+  }
+});
+
+app.get("/fetch-description", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    const url = req.query.url; // Get the URL from the query parameter
+    if (!url) {
+      res.status(400).json({ error: "URL parameter is required." });
+      return;
+    }
+
+    await page.goto(url);
+    const descriptionSelector = 'meta[name="description"]';
+
+    await page.waitForSelector(descriptionSelector); // Wait for the meta description tag to appear
+
+    const metaDescription = await page.$eval(descriptionSelector, (element) =>
+      element.getAttribute("content")
+    );
+
+    await browser.close();
+
+    res.json({
+      metaDescription,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred." });
+  }
+});
+
+// app.get("/fetch-media", async (req, res) => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   const url = req.query.url;
+//   try {
+//     let counter = 0;
+//     page.on("response", async (response) => {
+//       const matches = /.*\.(jpg|png|svg|gif)$/.exec(response.url());
+//       if (matches && matches.length === 2) {
+//         const extension = matches[1];
+//         const buffer = await response.buffer();
+//         fs.writeFileSync(
+//           `images/image-${counter}.${extension}`,
+//           buffer,
+//           "base64"
+//         );
+//         counter += 1;
+//       }
+//     });
+
+//     await page.goto(url);
+//     res.status(200).json({ success: "Images saved successfully" });
+//   } catch (error) {
+//     console.error("Error:", error);
+//     res.status(500).json({ error });
+//   } finally {
+//     await browser.close();
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
